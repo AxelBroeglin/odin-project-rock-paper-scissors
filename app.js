84b7bd9a851e22ad108a9cefa955e20427303371
AxelBroeglin <axelbroeglin@gmail.com>
2022-03-19 16:04:05 +0000

    Creation of the 3 HTML buttons in JS

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
-const possibleChoices = [
-    "Rock",
-    "Paper",
-    "Scissors"
-  ];
-  console.log(possibleChoices)
+//Creation of the array with all the possible choices
+ const possibleChoices = [
+     "Rock",
+     "Paper",
+     "Scissors"
+   ];
 
-  
-  let computerChoice = possibleChoices[Math.floor(Math.random()*possibleChoices.length)];
-  console.log(computerChoice)
+
+//Creation of the 3 buttons for the player to chose between
+let btn = ''
+possibleChoices.forEach(function(buttonCreation) {
+    btn += '<button type="button">'+ buttonCreation + '</button>';
+}); 
+
+document.getElementById("btnContainer").innerHTML = btn;
+
+
+ //Creation of the function that will randomize the computer's choices
+let computerChoice = possibleChoices[Math.floor(Math.random()*possibleChoices.length)];
+console.log(computerChoice)
+
+
+
