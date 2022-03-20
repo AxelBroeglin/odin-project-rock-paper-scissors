35130d40c1f5b2b565e2881ff203dc637d3b701c
AxelBroeglin <axelbroeglin@gmail.com>
2022-03-20 17:40:10 +0000

    Still trying to understand JS

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
+const computerChoiceDisplay = document.getElementById('computer-choice')
+const userChoiceDisplay = document.getElementById('user-choice')
+const resultDisplay = document.getElementById('result')
+const possibleChoice = document.querySelectorAll('button')
+let userChoice
+let result
+
 //Creation of the array with all the possible choices
 const possibleChoices = [
     "Rock",
     "Paper",
     "Scissors"
 ];
+console.log(possibleChoices)
+
 
 //Creation of the function that will randomize the computer's choices
 let computerChoice = possibleChoices[Math.floor(Math.random()*possibleChoices.length)];
 console.log(computerChoice)
+computerChoiceDisplay.innerHTML = computerChoice
 
 
 //Creation of the 3 buttons for the player to chose between
@@ -15,29 +25,29 @@ let btn = ''
 possibleChoices.forEach(function(buttonCreation) {
     btn += '<button type="button">'+ buttonCreation + '</button>';
 }); 
-btn.id = 'someId';
-
 document.getElementById("btnContainer").innerHTML = btn;
 
 
 //Creation of incremented IDs for the buttons to compare results
 let buttonId = document.querySelectorAll('button');
-var incrementedId = 1;
+let incrementedId = 1;
 for(var i=0; i< buttonId.length; i++){
     buttonId[i].id = incrementedId;
     incrementedId++;
 }
 
-
-//To edit
 //Everything that happens when we click
-possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+possibleChoice.forEach(possibleChoices => possibleChoices.addEventListener('click', (e) => {
     userChoice = e.target.id
     userChoiceDisplay.innerHTML = userChoice
-    generateComputerChoice()
+    computerChoice()
     getResult()
+
   }))
 
-
-
-
+  possibleChoice.onclick= function() {myFunction()}
+  
+  function myFunction(){
+      userChoiceDisplay.innerHTML = userChoice
+  }
+  myFunction()
